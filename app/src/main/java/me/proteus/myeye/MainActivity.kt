6c0f3d488837fe9b03d579703f5c9c2961daf24d
321Proteus <bgproteus@gmail.com>
2024-10-12 10:15:19 +0200

    Added stage system with scaling and randomizing

--- app/src/main/java/me/proteus/myeye/MainActivity.kt
+++ app/src/main/java/me/proteus/myeye/MainActivity.kt
@@ -4,11 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -28,7 +37,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyEyeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    generateText(8, modifier = Modifier.padding(innerPadding))
+                    SnellenChart(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -36,30 +45,49 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun generateText(length: Int, modifier: Modifier = Modifier) {
+fun SnellenChart(modifier: Modifier = Modifier) {
+
+    var stage: Int by remember { mutableStateOf(1) }
+    var text: String by remember { mutableStateOf(generateText(stage)) }
+
+    Column(
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Box (
+            modifier = modifier
+                .fillMaxSize()
+                .clickable(onClick = {
+                    if (stage < 8) stage++;
+                    text = generateText(stage)
+                    println(stage)
+                })
+                .padding(bottom = 16.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = text,
+                color = Color.Black,
+                fontSize = (27*(9-stage)).sp,
+                letterSpacing = 16.sp,
+                fontFamily = FontFamily(Font(R.font.opticiansans)),
+                modifier = modifier
+            )
+        }
+    }
+}
+
+fun generateText(stage: Int): String {
 
     var random = Random()
     var text: String = ""
 
-    for (i in 1..length) {
+    for (i in 1..stage) {
         text += ((abs(random.nextInt() % 25)) + 65).toChar()
     }
     println(text)
 
-    Text(
-        text = text,
-        color = Color.Black,
-        fontSize = 48.sp,
-        letterSpacing = 16.sp,
-        fontFamily = FontFamily(Font(R.font.opticiansans)),
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyEyeTheme {
-        generateText(8)
-    }
+    return text
 }
