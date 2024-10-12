5ddd80098798921cd5c3595210221182a9d0cfac
321Proteus <bgproteus@gmail.com>
2024-10-12 12:03:26 +0200

    Created menu screen

--- app/src/main/java/me/proteus/myeye/MainActivity.kt
+++ app/src/main/java/me/proteus/myeye/MainActivity.kt
@@ -1,32 +1,18 @@
 package me.proteus.myeye
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.*
 import me.proteus.myeye.ui.theme.MyEyeTheme
-import java.util.Random
-import kotlin.math.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,85 +21,27 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyEyeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    SnellenChart(modifier = Modifier.padding(innerPadding))
+
+                    var activityContext = this
+
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Button(
+                            modifier = Modifier.padding(innerPadding),
+                            onClick = {
+                                val intent = Intent(activityContext, SnellenChartActivity::class.java)
+                                startActivity(intent)
+                            }
+                        ) {
+                            Text(text = "Uruchom test")
+                        }
+                    }
+
                 }
             }
         }
     }
-}
-
-@Composable
-fun SnellenChart(modifier: Modifier = Modifier) {
-
-    var stage: Int by remember { mutableIntStateOf(1) }
-    var text: String by remember { mutableStateOf(generateText(stage)) }
-
-    Column(
-        modifier = Modifier.fillMaxSize().padding(16.dp),
-        verticalArrangement = Arrangement.SpaceBetween,
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-
-        Box (
-            modifier = modifier
-                .weight(1f)
-                .fillMaxWidth()
-                .padding(bottom = 16.dp),
-            contentAlignment = Alignment.Center,
-        ) {
-            Text(
-                text = text,
-                color = Color.Black,
-                fontSize = (27*(9-stage)).sp,
-                letterSpacing = 16.sp,
-                fontFamily = FontFamily(Font(R.font.opticiansans)),
-                modifier = modifier
-            )
-        }
-
-        ButtonRow(
-            onRegenerate = { text = generateText(stage) },
-            onSizeIncrease = { if (stage > 1) { stage--; text = generateText(stage)} },
-            onSizeDecrease = { if (stage < 10) { stage++; text = generateText(stage) } }
-        )
-    }
-}
-
-@Composable
-fun ButtonRow(
-    onRegenerate: () -> Unit,
-    onSizeIncrease: () -> Unit,
-    onSizeDecrease: () -> Unit,
-    modifier: Modifier = Modifier) {
-
-    Row(
-        modifier = modifier
-            .fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceEvenly,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Button(onClick = { onSizeIncrease() }) {
-            Text(text = "Wieksze litery")
-        }
-        Button(onClick = { onRegenerate() }) {
-            Text(text = "Losuj litery")
-        }
-        Button(onClick = { onSizeDecrease() }) {
-            Text(text = "Mniejsze litery")
-        }
-    }
-
-}
-
-fun generateText(stage: Int): String {
-
-    var random = Random()
-    var text: String = ""
-
-    for (i in 1..stage) {
-        text += ((abs(random.nextInt() % 25)) + 65).toChar()
-    }
-    println(text)
-
-    return text
 }
