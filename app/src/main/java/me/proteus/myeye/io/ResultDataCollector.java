2d072872015b55b0c3dc97afcf4c7f92b5020c97
321Proteus <bgproteus@gmail.com>
2024-11-19 16:40:49 +0100

    Added logic for storing stage results

--- app/src/main/java/me/proteus/myeye/io/ResultDataCollector.java
+++ app/src/main/java/me/proteus/myeye/io/ResultDataCollector.java
@@ -1,4 +1,21 @@
 package me.proteus.myeye.io;
 
+import android.util.Pair;
+
+import java.util.List;
+
 public class ResultDataCollector {
+    protected List<Pair<String, String>> stages;
+
+
+    public ResultDataCollector() {
+
+    }
+
+    public void addResult(Pair<String, String> p) {
+
+        stages.add(p);
+
+    }
+
 }
