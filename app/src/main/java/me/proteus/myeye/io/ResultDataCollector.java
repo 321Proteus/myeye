947b3f98ac44ed5f8974178099c1737b78cab369
321Proteus <bgproteus@gmail.com>
2024-11-19 17:33:22 +0100

    Fixed app crash on adding first stage result

--- app/src/main/java/me/proteus/myeye/io/ResultDataCollector.java
+++ app/src/main/java/me/proteus/myeye/io/ResultDataCollector.java
@@ -2,10 +2,11 @@ package me.proteus.myeye.io;
 
 import android.util.Pair;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ResultDataCollector {
-    protected List<Pair<String, String>> stages;
+    protected List<Pair<String, String>> stages = new ArrayList<>();
 
 
     public ResultDataCollector() {
