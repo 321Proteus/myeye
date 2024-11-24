06caa50defb2c8782b1616df6b0dd3d382a7758c
321Proteus <bgproteus@gmail.com>
2024-11-24 19:23:33 +0100

    Added incorrect test ID handling in result browser

--- app/src/main/java/me/proteus/myeye/ui/ResultBrowserActivity.kt
+++ app/src/main/java/me/proteus/myeye/ui/ResultBrowserActivity.kt
@@ -1,19 +1,27 @@
 package me.proteus.myeye
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import me.proteus.myeye.io.ResultDataSaver
 import me.proteus.myeye.ui.theme.MyEyeTheme
+import me.proteus.myeye.visiontests.VisionTestUtils
+import java.lang.IllegalArgumentException
 
 class ResultBrowserActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,26 +31,77 @@ class ResultBrowserActivity : ComponentActivity() {
             MyEyeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
 
-                    Column (
-                        modifier = Modifier
-                            .padding(innerPadding)
-                            .background(Color.LightGray)
-                    ) {
-                        val dbConnector = ResultDataSaver(this@ResultBrowserActivity)
-                        dbConnector.select("*")
-
-                        val ids: List<Int> = dbConnector.idList;
-
-                        for (i in ids) print("$i ")
-                        println()
-
-                        for (i in ids) {
-                            Text(text = dbConnector.testNames[i-1])
-                        }
-                    }
+                    ResultColumn(this@ResultBrowserActivity, innerPadding)
 
                 }
             }
         }
     }
+}
+
+@Composable
+fun ResultColumn(activity: ResultBrowserActivity, paddingValues: PaddingValues) {
+
+    val dbConnector = ResultDataSaver(activity)
+    dbConnector.select("*")
+
+    val ids: List<Int> = dbConnector.idList;
+
+    for (i in ids) print("$i ")
+    println()
+
+    Column (
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(paddingValues),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+
+        for (i in ids) {
+
+            var testID = dbConnector.testNames[i-1]
+            var boxTitle: String;
+
+            try {
+
+                boxTitle = VisionTestUtils().getTestTypeByID(testID) + " " + VisionTestUtils().getTestNameByID(testID)
+
+            } catch (e: IllegalArgumentException) {
+
+                boxTitle = "Nieznany test"
+                Log.w(activity.localClassName, "Nieprawidlowe ID testu: '$testID' na pozycji $i")
+            }
+
+
+            Box(
+                modifier = Modifier
+                    .height(60.dp)
+                    .fillMaxWidth()
+                    .background(Color.LightGray)
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .padding(vertical = 8.dp, horizontal = 16.dp)
+                            .aspectRatio(1.0f)
+                            .clip(shape = RoundedCornerShape(15.dp))
+                            .background(Color.Blue)
+                    )
+                    Text(
+                        text = boxTitle,
+                        fontSize = 18.sp
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.padding(8.dp))
+        }
+
+    }
+
+
+
 }
