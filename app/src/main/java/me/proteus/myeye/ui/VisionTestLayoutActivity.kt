c47ed00865497c4965664076c28d3a5fda2ae057
321Proteus <bgproteus@gmail.com>
2024-11-26 13:09:52 +0100

    Added result renderer intent input to layout activity

--- app/src/main/java/me/proteus/myeye/ui/VisionTestLayoutActivity.kt
+++ app/src/main/java/me/proteus/myeye/ui/VisionTestLayoutActivity.kt
@@ -9,9 +9,11 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.core.content.IntentCompat
 import me.proteus.myeye.VisionTest
 import me.proteus.myeye.ui.theme.MyEyeTheme
 import me.proteus.myeye.visiontests.VisionTestUtils
+import me.proteus.myeye.TestResult
 
 class VisionTestLayoutActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,7 +32,13 @@ class VisionTestLayoutActivity : ComponentActivity() {
                         contentAlignment = Alignment.Center
                     ) {
 
-                        getTest(intent).BeginTest(activity = this@VisionTestLayoutActivity, modifier = Modifier, isResult = false, result = null)
+                        var isResult: Boolean = intent.getBooleanExtra("IS_RESULT", false)
+
+                        var resultData: TestResult? = IntentCompat.getParcelableExtra(intent, "RESULT_PARCEL", TestResult::class.java)
+
+                        println(if (resultData != null) "Znaleziono TestResult o ID ${resultData.resultID}" else "Nie przekazano TestResult")
+
+                        getTest(intent).BeginTest(activity = this@VisionTestLayoutActivity, modifier = Modifier, isResult = isResult, result = resultData)
 
                     }
 
