554f73cdc5c1824bf20cb7aff1e635b2e9702a45
321Proteus <bgproteus@gmail.com>
2024-11-28 22:13:07 +0100

    Created basic grid display for Farnsworth

--- app/src/main/java/me/proteus/myeye/visiontests/ColorArrangementTest.kt
+++ app/src/main/java/me/proteus/myeye/visiontests/ColorArrangementTest.kt
@@ -2,19 +2,44 @@ package me.proteus.myeye.visiontests
 
 import android.graphics.Color.parseColor
 import android.graphics.Color.colorToHSV
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.gestures.rememberDraggableState
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.List
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.material3.Button
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
 import me.proteus.myeye.R
 import me.proteus.myeye.SerializablePair
 import me.proteus.myeye.TestResult
 import me.proteus.myeye.VisionTest
 import me.proteus.myeye.io.ResultDataCollector
 import me.proteus.myeye.ui.VisionTestLayoutActivity
+import kotlin.math.max
 
 class ColorArrangementTest : VisionTest {
     override val testID: String = "COLOR_ARRANGE"
@@ -31,6 +56,54 @@ class ColorArrangementTest : VisionTest {
         stages: List<SerializablePair>,
         isResult: Boolean
     ) {
+
+        var stageIterator: Int by remember { mutableIntStateOf(1) }
+
+        var shuffledStageColors by remember { mutableStateOf(stages[stageIterator-1].second.split(" ")) }
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxHeight(0.9f),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceEvenly
+            ) {
+                 itemsIndexed(shuffledStageColors) { index, item ->
+
+                    Column(
+                        modifier = Modifier
+
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth(0.4f)
+                                .height(60.dp)
+                                .background(Color(parseColor(item))),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text("${getHue(item)}")
+                        }
+                    }
+
+                }
+            }
+
+            Box(
+                contentAlignment = Alignment.Center
+            ) {
+                Button(
+                    onClick = {
+                        if (stageIterator < stageCount) stageIterator++
+                        else println("I po tescie")
+                    }
+                ) {
+                    Text("Dalej")
+                }
+            }
+        }
+
 
 
     }
@@ -77,6 +150,16 @@ class ColorArrangementTest : VisionTest {
 
     override fun checkAnswer(answer: String): Boolean {
         TODO("Not yet implemented")
+    }
+
+    fun getHue(color: String): Float {
+
+        var hsv = FloatArray(3)
+        var a = Color(parseColor(color)).toArgb()
+
+        colorToHSV(a, hsv)
+
+        return hsv[0]
     }
 
 
