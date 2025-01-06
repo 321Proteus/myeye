86dc98d75c5f8c6636a926b0f1916e06a29a8086
321Proteus <bgproteus@gmail.com>
2025-01-06 19:38:27 +0100

    Library version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "me.proteus.myeye"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "me.proteus.myeye"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
