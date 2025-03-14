771c3f9b145cea7323aaba52bf789f3f8377b533
321Proteus <bgproteus@gmail.com>
2025-03-14 14:06:54 +0100

    Improved place details format and styling

--- composeApp/src/wasmJsMain/resources/place.js
+++ composeApp/src/wasmJsMain/resources/place.js
@@ -28,12 +28,34 @@ async function fetchPlace(id) {
 }
 
 function displayPlaceData(el, data) {
+    console.log(data);
     el.getElementById("place-name").textContent = data.Eg.displayName;
-    el.getElementById("place-address").textContent = data.Eg.formattedAddress;
-    el.getElementById("place-hours").textContent = "todo";
+    el.getElementById("place-address").textContent = "Adres: " + data.Eg.formattedAddress;
     el.getElementById("place-id").textContent = data.id;
-    el.getElementById("place-web-page").textContent = data.Eg.websiteURI;
-    el.getElementById("place-phone").textContent = data.Eg.nationalPhoneNumber;
+
+    var url = data.Eg.websiteURI;
+    if (url) {
+        url = url.substr(0, url.includes('/') ? url.lastIndexOf('/') : url.length);
+        el.getElementById("place-web-page").innerHTML = `WWW: <a href=${url}>${url}</>`;
+    }
+
+    el.getElementById("place-phone").textContent = "Telefon: " + data.Eg.nationalPhoneNumber;
+
+    const hoursDiv = el.getElementById("place-hours");
+    const hoursData = data.Eg.regularOpeningHours;
+
+    const now = new Date();
+    const period = hoursData.periods[now.getDay()-1];
+    const isOpen = period && // w periods nie ma dni kiedy jest niczynne
+        now.getHours() >= period.open.hour &&
+        now.getHours() < period.close.hour;
+
+    hoursDiv.innerHTML += `Godziny otwarcia (obecnie ${isOpen ? "otwarte" : "zamknięte"}):`
+    console.log(hoursData)
+    hoursData.weekdayDescriptions.forEach(day => {
+        hoursDiv.innerHTML += "<br>" + day;
+    })
+
 }
 
 async function showPlace(id) {
@@ -41,7 +63,7 @@ async function showPlace(id) {
     var placeDocument = await loadPlacePage()
 
     const mapDiv = document.getElementById("map");
-    mapDiv.style.width = "50%";
+    mapDiv.style.width = "60%";
 
     const placeData = await fetchPlace(id);
     displayPlaceData(placeDocument, placeData);
@@ -58,8 +80,5 @@ async function showPlace(id) {
 
     placeDiv.id = "place";
     document.body.appendChild(placeDiv);
-
-
-    // placeDiv.innerHTML = `<div>${id}</div>`
 
 }
