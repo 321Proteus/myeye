15371c6adaf87c3c8045c070f93b0b370c954b69
321Proteus <bgproteus@gmail.com>
2025-03-22 11:11:45 +0100

    Implemented ASR on iOS

--- iosApp/swiftSrc/NativeRecognizer.swift
+++ iosApp/swiftSrc/NativeRecognizer.swift
@@ -8,16 +8,14 @@
 
 import Foundation
 import Speech
+import MyLibrary
 
-@objc public class SpeechRecognizer: NSObject, SFSpeechRecognizerDelegate {
-    private let recognizer = SFSpeechRecognizer(locale: Locale(identifier: "pl-PL"))!
-    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
-    private var recognitionTask: SFSpeechRecognitionTask?
-    private let audioEngine = AVAudioEngine()
+@objc public class NativeRecognizer: NSObject, SFSpeechRecognizerDelegate {
     
-    private var history: [String] = []
-    private var chunk: [String] = []
-    private var silenceTimer: Timer?
+    var audioEngine : AVAudioEngine!
+    var processingQueue: DispatchQueue!
+    var model: VoskModel?
+    var recognizer: VoskRecognizer?
     
     @objc public static func requestAuthorization(callback: @escaping (Bool) -> Void) {
         SFSpeechRecognizer.requestAuthorization { status in
@@ -27,71 +25,69 @@ import Speech
         }
     }
 
-    @objc public func startRecognition(grammar: [String], callback: @escaping (String?) -> Void) {
-        if recognitionTask != nil {
-            recognitionTask?.cancel()
-            recognitionTask = nil
+    @objc public func startRecognition(grammar: String, path: String, callback: @escaping (String?) -> Void) {
+        self.processingQueue = DispatchQueue(label: "processingQueue")
+        let session = AVAudioSession.sharedInstance()
+        do {
+            try session.setCategory(.playAndRecord)
+            try session.setPreferredSampleRate(48000)
+            try session.setActive(true)
+        } catch {
+            print("Failed to set the audio session configuration")
         }
         
-        history.removeAll()
-
-        let audioSession = AVAudioSession.sharedInstance()
-        try? audioSession.setCategory(.record, mode: .measurement, options: .duckOthers)
-        try? audioSession.setActive(true, options: .notifyOthersOnDeactivation)
-        
-        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
-        recognitionRequest?.contextualStrings = grammar
-        let inputNode = audioEngine.inputNode
-        recognitionRequest?.shouldReportPartialResults = true
-        
-        recognitionTask = recognizer.recognitionTask(with: recognitionRequest!) { result, error in
-            if let result = result {
-                let words = result.bestTranscription.segments.map { $0.substring }
-                let newWords = words.dropFirst(self.history.count)
-
-                if !newWords.isEmpty {
-                    self.chunk.append(contentsOf: newWords)
-                    self.resetTimer(callback)
+        session.requestRecordPermission { [self] granted in
+            do {
+                
+                audioEngine = AVAudioEngine()
+                
+                let inputNode = audioEngine.inputNode
+                let formatInput = inputNode.inputFormat(forBus: 0)
+                
+                let rate = formatInput.sampleRate
+                print(rate)
+                
+                let format = AVAudioFormat.init(commonFormat: AVAudioCommonFormat.pcmFormatInt16, sampleRate: rate, channels: 1, interleaved: false)
+                
+                model = VoskModel(path: path, spkPath: nil)
+                recognizer = VoskRecognizer(model: model!, sampleRate: Float(rate), grammar: grammar)
+                
+                print("Volume: \(session.outputVolume)")
+                print("Route: \(session.currentRoute)")
+                
+                inputNode.installTap(
+                    onBus: 0,
+                    bufferSize: UInt32(formatInput.sampleRate / 10),
+                    format: format
+                ) { buffer, time in self.processingQueue.async {
+                    let res = self.recognizer!.recognizeData(buffer: buffer)
+                    DispatchQueue.main.async {
+                        if (!res.contains("\"\"") && res != "") {
+                            AudioServicesPlaySystemSound(1025)
+                            callback(res)
+                        }
+                    }
                 }
-
-                self.history = words
-            } else if let error = error {
-                print("Błąd: \(error.localizedDescription)")
-                callback(nil)
+                }
+                
+                audioEngine.prepare()
+                try audioEngine.start()
+            } catch {
+                print("Unable to start AVAudioEngine: \(error.localizedDescription)")
             }
         }
-
-        let recordingFormat = inputNode.outputFormat(forBus: 0)
-        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer, _) in
-            self.recognitionRequest?.append(buffer)
-        }
-        
-        audioEngine.prepare()
-        try? audioEngine.start()
-    }
-
-    @objc public func stopRecognition() {
-        audioEngine.stop()
-        recognitionRequest?.endAudio()
     }
     
-    private func resetTimer(_ callback: @escaping (String) -> Void) {
-        silenceTimer?.invalidate()
-        
-        silenceTimer = Timer.scheduledTimer(withTimeInterval: 1.2, repeats: false) { _ in
-            if !self.chunk.isEmpty {
-                let chunkText = self.chunk.joined(separator: " ")
-                callback(chunkText)
-                
-                self.chunk.removeAll()
-                self.playSound()
-            }
+    @objc public func stopRecognition() {
+        model = nil
+        recognizer = nil
+        if audioEngine.isRunning {
+            audioEngine.stop()
         }
+        audioEngine.inputNode.removeTap(onBus: 0)
+        
+        processingQueue = nil
     }
 
-    private func playSound() {
-        let systemSoundID: SystemSoundID = 1057
-        AudioServicesPlaySystemSound(systemSoundID)
-    }
 }
 
